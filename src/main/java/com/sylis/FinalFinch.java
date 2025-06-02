176f2db7a4d12e2abfa307589811f069eaf83409
Sylis818 <snoor0001@mymail.lausd.net>
2025-06-02 13:19:06 -0700

    e

--- src/main/java/com/sylis/FinalFinch.java
+++ src/main/java/com/sylis/FinalFinch.java
@@ -1,12 +1,20 @@
 package com.sylis;
 import com.birdbrain.Finch;
-public class SquareFinch {
-public static void main(Strings[] args) {
+public class FinalFinch {
+public static void main(String[] args) {
     Finch bird = new Finch();
+
+
+
 for (int i = 0; i<4; i++) {
-bird.setMove("F", 20, 100);
+bird.setMove("F", 5, 100);
 bird.setTurn("R", 90, 100);
+bird.playNote(32,4);
 }
+
+
+
+
 
     bird.stop();
     bird.disconnect();
