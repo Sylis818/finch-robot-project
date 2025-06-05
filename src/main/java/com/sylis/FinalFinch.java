df4499c01603e4a79e7136d1b1307b8c75600e03
Sylis818 <snoor0001@mymail.lausd.net>
2025-06-05 09:30:14 -0700

    Update FinalFinch.java

--- src/main/java/com/sylis/FinalFinch.java
+++ src/main/java/com/sylis/FinalFinch.java
@@ -6,25 +6,44 @@ public static void main(String[] args) {
 
 
 
-
+//move through the first straight part
 bird.setMove("F", 100, 80);
+//takes the first turn
 bird.setTurn("L", 90, 100);
+//plays a note because why not
 bird.playNote(32,4);
+//moves it a little bit 
 bird.setMove("F", 30, 80);
+//turns 
 bird.setTurn("L", 90, 100);
-bird.setMove("F", 40, 80);
+//moves to line up with the gap
+bird.setMove("F", 35, 80);
+//turns to face the gap
 bird.setTurn("R", 90, 100);
+//moves through the gap
 bird.setMove("F", 60, 80);
+//turns to the right
+bird.setTurn("R", 96, 100);
+//goes through the long part
+bird.setMove("F", 125, 60);
+//tunrs right
 bird.setTurn("R", 90, 100);
-bird.setMove("F", 120, 80);
-bird.setTurn("R", 90, 100);
-bird.setMove("F", 10, 80);
+//moves a little
+bird.setMove("F", 15, 80);
+//turns left
+bird.setTurn("L", 90, 100);
+//moves a little bit to make the last turn
+bird.setMove("F", 25, 80);
+//last turn
+bird.setTurn("L", 90, 100);
+//goes to the end
+bird.setMove("F", 20, 80);
 
 
 
 
 
-
+//stops the bird and disconnects it from the program
     bird.stop();
     bird.disconnect();
 }
